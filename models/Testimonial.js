7677c94167dc402eb17f7d3eb4b60f48efb3b784
shubham18102000 <shubhamchamoli8218035988@gmail.com>
2025-08-28 17:39:57 +0530

    add

--- models/Testimonial.js
+++ models/Testimonial.js
@@ -14,7 +14,9 @@ const mongoose = require("mongoose");
 
 const testimonialSchema = new mongoose.Schema({
   name: String,
-  message: String,
+  review: { type: String, required: true },   // user review text
+  stars: { type: Number, required: true },    // rating stars
+  photo: { type: String },                    // Cloudinary URL
   approved: { type: Boolean, default: false },
   visible: { type: Boolean, default: true },  // 👈 added field
   createdAt: { type: Date, default: Date.now }
